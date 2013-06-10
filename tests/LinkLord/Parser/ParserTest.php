ffe6844006af140bf42a6c3422e110958b120d61
Ricardo Cruz <piradoiv@gmail.com>
2013-06-10 14:47:09 +0100

    Add more tests to work on Tomorrow

--- tests/LinkLord/Parser/ParserTest.php
+++ tests/LinkLord/Parser/ParserTest.php
@@ -6,7 +6,10 @@ use PiradoIV\Html\LinkLord\Parser;
 
 class ParserTest extends \PHPUnit_Framework_TestCase
 {
-    private $html = '<p>Lorem ipsum <a href="#dolor" target="_blank">dolor</a> sit amet</p>';
+    private $html = <<<HTML
+        <p>Lorem ipsum <a href="#dolor" target="_blank">dolor</a> sit amet</p>
+        <p>Wop <a href="#image"><img src="wop.png" /></a></p>
+HTML;
     
     public function testParserCanExtractsLinksFromString()
     {
@@ -14,5 +17,23 @@ class ParserTest extends \PHPUnit_Framework_TestCase
         $links = $parser->getLinks();
 
         $this->assertNotEmpty($links);
+    }
+
+    public function testParsedLinksHaveNofollowAttribute()
+    {
+        $parser = new Parser($this->html);
+        $links = $parser->getLinks();
+
+        foreach ($links as $link) {
+            $this->assertNotNull($link->isNoFollow);
+        }
+    }
+
+    public function testParsedLinksDetectsIfIsAnImage()
+    {
+        $parser = new Parser($this->html);
+        $links = $parser->getLinks();
+
+        $this->assertTrue($links[1]->isImage, true);
     }
 }
