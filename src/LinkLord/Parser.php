98fd37819f8a50811fca6c1aa17736980ece8ea9
Ricardo Cruz <piradoiv@gmail.com>
2013-06-10 14:38:28 +0100

    Add Parser functionality to extract links

--- src/LinkLord/Parser.php
+++ src/LinkLord/Parser.php
@@ -2,7 +2,27 @@
 
 namespace PiradoIV\Html\LinkLord;
 
+use \Symfony\Component\DomCrawler\Crawler;
+
 class Parser
 {
-    
+    private $crawler;
+    private $links;
+
+    public function __construct($string = '')
+    {
+        $this->crawler = new Crawler($string);
+        $this->links = array();
+    }
+
+    public function getLinks()
+    {
+        $this->crawler->filter('a')->each(
+            function ($node, $i) {
+                array_push($this->links, $node);
+            }
+        );
+
+        return $this->links;
+    }
 }
